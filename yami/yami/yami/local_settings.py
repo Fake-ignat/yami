41a5fc4a8b1f3f35f1cf89cf60c4303f336512b5
M.Shabanov <deus7@protonmail.com>
2021-07-09 11:45:15 +0300

    Подготовка к деплою

---
+++ yami/yami/yami/local_settings.py
@@ -0,0 +1,19 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'Number1992'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
